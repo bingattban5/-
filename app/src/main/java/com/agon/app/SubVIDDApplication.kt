5d236fbfbd875a01633ac70f9bc0b3102f2e8af4
bingattban5 <bingattban5@gmail.com>
2026-08-04 02:13:19 +0300

    Update SubVIDDApplication.kt

--- app/src/main/java/com/agon/app/SubVIDDApplication.kt
+++ app/src/main/java/com/agon/app/SubVIDDApplication.kt
@@ -2,14 +2,21 @@ package com.agon.app
 
 import android.app.Application
 import android.util.Log
+import androidx.work.Configuration
+import androidx.work.WorkManager
 import com.yausername.ffmpeg.FFmpeg
 import com.yausername.youtubedl_android.YoutubeDL
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SubVIDDApplication : Application() {
+class SubVIDDApplication : Application(), Configuration.Provider {
+    
     override fun onCreate() {
         super.onCreate()
+        
+        // تهيئة WorkManager يدوياً (مهم جداً!)
+        WorkManager.initialize(this, workManagerConfiguration)
+        
         try {
             // تهيئة بيئة yt-dlp و FFmpeg للعمل داخل التطبيق
             YoutubeDL.getInstance().init(this)
@@ -18,4 +25,10 @@ class SubVIDDApplication : Application() {
             Log.e("SubVIDDApplication", "Failed to initialize yt-dlp or ffmpeg", e)
         }
     }
+
+    // إعدادات WorkManager
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setMinimumLoggingLevel(Log.INFO)
+            .build()
 }
